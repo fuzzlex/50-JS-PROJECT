f91650e224bcc0627be51b8f1607a9c64830253b
emre <emrky.2018@icloud.com>
2021-11-17 18:31:37 +0300

    update

--- Image_Flipper/js/script.js
+++ Image_Flipper/js/script.js
@@ -1,14 +1,27 @@
 const buttons = document.querySelectorAll("button");
+let arr = [];
+let x = 0;
 
+for(i = 0;i < 10;i++){
+    let num = Math.floor(Math.random() * 100) ;
+    arr.push(num);
+}
 buttons.forEach((e)=>{
+
     e.addEventListener("click", ()=>{
-        let num = Math.floor(Math.random() * 100) 
+
+
+        
 
         if(e.textContent == "NEXT"){
-            e.parentElement.parentElement.style.background = `url('https://picsum.photos/id/${num}/600/400') no-repeat center`;
+            e.parentElement.parentElement.style.background = `url('https://picsum.photos/id/${arr[x]}/600/400') no-repeat center`;
+            x++;
+            if(x == arr.length)x = 0;
         }
         else if(e.textContent == "PREVIOUS"){
-            e.parentElement.parentElement.style.background = `url('https://picsum.photos/id/${num-1}/600/400') no-repeat center`;
+            if(x > 0)x--;
+            if(x == 0)x = arr.length;
+            e.parentElement.parentElement.style.background = `url('https://picsum.photos/id/${arr[x-1]}/600/400') no-repeat center`;
         }
     })
 })
